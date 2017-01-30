81bd1253cfdd871716e80546b3ba94e2cb47b5e7
ryosukes <nu.ciger@gmail.com>
2017-01-30 23:43:32 +0900

    load toml function

--- main.go
+++ main.go
@@ -7,7 +7,24 @@ import (
     "github.com/urfave/cli"
     "golang.org/x/oauth2"
     "github.com/google/go-github/github"
+    "github.com/BurntSushi/toml"
 )
+
+type Config struct {
+    User UserConfig
+    Repositories []RepositoryConfig
+}
+
+type UserConfig struct {
+    Token string    `toml:"token"`
+}
+
+type RepositoryConfig struct {
+    Owner   string  `toml:"owner"`
+    Name    string  `toml:"name"`
+}
+
+var config Config
 
 func main() {
     app := cli.NewApp()
@@ -18,24 +35,32 @@ func main() {
         return nil
     }
 
+    loadConfig()
     findIssues()
 
     app.Run(os.Args)
 }
 
 func findIssues() {
-    const token = "hoge"
+    var token  = config.User.Token
 
     ts := oauth2.StaticTokenSource(
         &oauth2.Token{AccessToken: token},
     )
     tc := oauth2.NewClient(oauth2.NoContext, ts)
     client := github.NewClient(tc)
-    issues, _, err := client.Issues.ListByRepo("fuga", "piyo", nil)
+    issues, _, err := client.Issues.ListByRepo(config.Repositories[0].Owner, config.Repositories[0].Name, nil)
 
     if err != nil {
         panic(err)
     }
 
     fmt.Println(issues)
+}
+
+func loadConfig() {
+    _, err := toml.DecodeFile("./config.tml", &config)
+    if err != nil {
+        panic(err)
+    }
 }
